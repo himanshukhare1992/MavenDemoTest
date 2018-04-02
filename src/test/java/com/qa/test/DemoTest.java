5280b93b20cc45eac1a384d7134281eb7459c8e6
Himanshu Khare <himanshukhare1992@gmail.com>
2018-04-02 23:50:45 +0530

    This is my first code commit..

---
+++ src/test/java/com/qa/test/DemoTest.java
@@ -0,0 +1,47 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+	@Test
+	public void sum()
+	{
+		System.out.println("===SUM Method===");
+		int a=10; 
+		int b =20;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	
+	@Test
+	public void mul()
+	{
+		System.out.println("===Multiplication Method===");
+		int a=10; 
+		int b=20;
+		Assert.assertEquals(200, a*b);
+	}
+	
+	
+	@Test
+	public void sub()
+	{
+		System.out.println("===Subtraction Method===");
+		int a=10; 
+		int b=20;
+		Assert.assertEquals(10, b-a);
+	}
+	
+	@Test
+	public void div()
+	{
+		System.out.println("===Divison Method===");
+		int a=10; 
+		int b=20;
+		Assert.assertEquals(2, b/a);
+	}
+	
+	
+}
